fb2aafb1f2e6d7356549952d64129137df2c5450
murtaza <murtazaezzy1001@gmail.com>
2022-09-15 09:47:03 +0530

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require('http');
+const path = require('path');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || "8000";
+
+const peerServer = ExpressPeerServer(server, {
+    proxied: true,
+    debug: true,
+    path: '/internet-phone',
+    port: 443,
+    ssl: {}
+});
+
+app.use(peerServer);
+server.listen(port, () => {
+    console.log(`server is listening on : ${port}`);
+});
---
+++ system_scripts/run_nodejs_development_server.bat
@@ -0,0 +1,2 @@
+cd C:\xampp\htdocs\all\nodejs\expressjs\internet-phone
+node server.js
