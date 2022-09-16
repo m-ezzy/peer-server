627b9c6443a0f2659ea396c146b099d90fd70c74
murtaza <murtazaezzy1001@gmail.com>
2022-09-16 17:58:47 +0530

    8

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const server = http.createServer(app);
 const port = process.env.PORT || "8000";
 
 const peerServer = ExpressPeerServer(server, {
-    allow_discovery: true,
-    proxied: true,
-    debug: true,
-    path: '/internet-phone',
-    port: 443,
-    //ssl: {},
-    secure: true
+	allow_discovery: true,
+	proxied: true,
+	debug: true,
+	path: '/internet-phone',
+	port: 80,
+	//ssl: {},
+	secure: true
 });
 
 app.use(peerServer);
 server.listen(port, () => {
-    console.log(`server is listening on : ${port}`);
+	console.log(`server is listening on : ${port}`);
 });
